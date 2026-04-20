e15da385e91b9f93c2bc59ec03cb2c6977339990
kyher <kyle.heron@pm.me>
2026-04-20 17:06:16 +0100

    Add tests

---
+++ tests/Unit/Teams/DeleteTeamActionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Unit\Teams;
+
+use App\Actions\Teams\DeleteTeam;
+use App\Models\Team;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DeleteTeamActionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_it_deletes_the_team(): void
+    {
+        $team = Team::factory()->create();
+
+        (new DeleteTeam)->handle($team);
+
+        $this->assertDatabaseMissing('teams', ['id' => $team->id]);
+    }
+}
