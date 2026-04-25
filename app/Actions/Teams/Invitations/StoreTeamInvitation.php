e8a3b38cdb7802d3cbca5b63f2212bdf35768af9
kyher <kyle.heron@pm.me>
2026-04-25 10:27:15 +0100

    Store team => Move validation to controller, not action

--- app/Actions/Teams/Invitations/StoreTeamInvitation.php
+++ app/Actions/Teams/Invitations/StoreTeamInvitation.php
@@ -6,24 +6,11 @@ use App\Enums\InvitationStatus;
 use App\Models\Team;
 use App\Models\TeamInvitation;
 use App\Models\User;
-use Illuminate\Validation\ValidationException;
 
 class StoreTeamInvitation
 {
     public function handle(Team $team, User $invitedBy, User $invitee): TeamInvitation
     {
-        if ($team->users()->where('users.id', $invitee->id)->exists()) {
-            throw ValidationException::withMessages([
-                'email' => __('This user is already a member of the team.'),
-            ]);
-        }
-
-        if ($team->invitations()->where('user_id', $invitee->id)->where('status', InvitationStatus::Pending)->exists()) {
-            throw ValidationException::withMessages([
-                'email' => __('This user already has a pending invitation.'),
-            ]);
-        }
-
         return TeamInvitation::create([
             'team_id' => $team->id,
             'invited_by_user_id' => $invitedBy->id,
--- app/Http/Controllers/Teams/Invitations/StoreTeamInvitationController.php
+++ app/Http/Controllers/Teams/Invitations/StoreTeamInvitationController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Teams\Invitations;
 
 use App\Actions\Teams\Invitations\StoreTeamInvitation;
+use App\Enums\InvitationStatus;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Teams\Invitations\StoreTeamInvitationRequest;
 use App\Models\Team;
@@ -10,6 +11,7 @@ use App\Models\TeamInvitation;
 use App\Models\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\ValidationException;
 use Inertia\Inertia;
 
 class StoreTeamInvitationController extends Controller
@@ -19,6 +21,18 @@ class StoreTeamInvitationController extends Controller
         Gate::authorize('create', [TeamInvitation::class, $team]);
 
         $invitee = User::where('email', $request->validated('email'))->firstOrFail();
+
+        if ($team->users()->where('users.id', $invitee->id)->exists()) {
+            throw ValidationException::withMessages([
+                'email' => __('This user is already a member of the team.'),
+            ]);
+        }
+
+        if ($team->invitations()->where('user_id', $invitee->id)->where('status', InvitationStatus::Pending)->exists()) {
+            throw ValidationException::withMessages([
+                'email' => __('This user already has a pending invitation.'),
+            ]);
+        }
 
         $action->handle($team, $request->user(), $invitee);
 
