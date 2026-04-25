74476831b6454f96a80dc7db55aa113de2ad58c1
kyher <kyle.heron@pm.me>
2026-04-25 10:28:04 +0100

    Remove unnecessary authorize fns in requests (policy protected)

--- app/Http/Requests/Teams/Invitations/StoreTeamInvitationRequest.php
+++ app/Http/Requests/Teams/Invitations/StoreTeamInvitationRequest.php
@@ -7,11 +7,6 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class StoreTeamInvitationRequest extends FormRequest
 {
-    public function authorize(): bool
-    {
-        return true;
-    }
-
     /**
      * @return array<string, ValidationRule|array<mixed>|string>
      */
